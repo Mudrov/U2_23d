d56f845b7f0940c18303b071fe0a782a6f4ed737
Pavel Mudrov <pavel.s.mudrov@gmail.com>
2023-02-18 08:57:14 +0200

    v 0.3

---
+++ js/js.js
@@ -0,0 +1,31 @@
+"use strict"
+//et nL = '';
+//const length = 10;
+
+//for (let i = 1; i < length; i++){
+//    
+//    for(let j = 0; j < i; j++){
+//        nL += "--";
+//    }
+
+//  nL = '\n';
+//    console.log(nL);
+//}
+//console.log(nL);
+//for (let i = 5; i <= 10; i++){
+  //  console.log(i);
+//}
+
+//for (let i =20; i > 10; i--){
+//    console.log(i);
+//    if (i == 13) {
+//        break;
+//    }
+//}
+  
+for (let i = 2; i<=10; i++){
+    if (i%2 !== 0){
+        continue;
+    }
+    console.log(i);
+}
